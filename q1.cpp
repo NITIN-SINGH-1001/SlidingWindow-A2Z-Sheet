8faac78f753c84c8f64beebbeee6085d81e6a7a5
Nitin Singh <nitin.pro.004@gmail.com>
2025-12-03 11:28:25 +0530

    1

---
+++ q1.cpp
@@ -0,0 +1,31 @@
+// solved on "Code 360"
+
+#include<bits/stdc++.h>
+
+int kDistinctChars(int k, string &str)
+{
+    // Write your code here
+    unordered_map<int,int>mp;
+    int ans=0;
+    
+    int l=0,r=0,n=str.size();
+    while(r < n)
+    {
+        mp[str[r]]++;
+
+        if(mp.size() > k)
+        {
+            mp[str[l]]--;
+            if(mp[str[l]]==0)
+            {
+                mp.erase(str[l]);
+            }
+            l++;
+        }
+        ans=max(ans,(r-l+1));
+        r++;
+    }
+    return ans;
+}
+
+
